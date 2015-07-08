03c09963c400b50747f1acca8b0ec78a6fb70b29
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-07-08 17:58:29 -0400

    Prepare for v0.10.3 Release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='AcornAccounting',
-    version='0.10.2',
+    version='0.10.3',
     author='Pavan Rikhi',
     author_email='pavan.rikhi@gmail.com',
     packages=find_packages,
